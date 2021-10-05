eb9344db4d2b280bdc7f783f8fabff6756dc0ec2
Victor Zamudio <victor.zamudio@rabbitmx.com>
2021-10-05 17:29:40 -0500

    fix(smr): testing semantic versioning poc

--- .github/workflows/release.config.js
+++ .github/workflows/release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: [{ name: 'main' }],
+    branches: "main",
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
